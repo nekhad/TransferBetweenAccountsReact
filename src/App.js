c9ca250b725abf66ffb71eed77e32bd0d37df6ad
nekhad <nihad.aliyev.2021@inbox.ru>
2024-02-01 17:52:24 +0400

    update some files :+1:

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // App.js
 
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 
 
 import Login from './Login';
@@ -8,9 +8,13 @@ import Registration from './Registration';
 import Dashboard from './Dashboard';
 import { Route, Router, Routes } from 'react-router-dom';
 import VerifyPage from './VerifyPage';
+const AuthContext=React.createContext(
+
+);
 
 const App = () => {
   const [loggedIn, setLoggedIn] = useState(false);
+  const [sendData, setSendData] = useState('');
   const [data, setData] = useState({
     name:'salam',
     onchange: (sa) => {
@@ -30,15 +34,19 @@ const App = () => {
   };
 
   return (
-
+<AuthContext.Provider value={{sendData,setSendData}}>
 <Routes>
   <Route path="/login" element={<Login data={data}/>}/>
   <Route path="/registration" element={<Registration/>}/>
   <Route path="/dashboard" element={<Dashboard/>}/>
   <Route path="/verify" element={<VerifyPage/>}/>
 </Routes>
-
+</AuthContext.Provider>
   );
 };
 
 export default App;
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
