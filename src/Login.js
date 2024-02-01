430e3b8110d39d0faf23d865799cb239957f1191
nekhad <nihad.aliyev.2021@inbox.ru>
2024-02-01 17:52:28 +0400

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
