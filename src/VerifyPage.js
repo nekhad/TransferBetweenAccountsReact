5ba84fc7992790fbfbeb00855e6e40bec5e07148
nekhad <nihad.aliyev.2021@inbox.ru>
2024-02-01 17:52:34 +0400

    Update VerifyPage.js

--- src/VerifyPage.js
+++ src/VerifyPage.js
@@ -1,58 +1,19 @@
-// import React, { useState } from 'react';
-// import axios from 'axios';
-// import { useNavigate } from 'react-router-dom';
-
-// const VerifyPage = () => {
-//   const [verificationCode, setVerificationCode] = useState('');
-//   const navigate = useNavigate();
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     try {
-//       // Make a POST request to the verification API
-//       const response = await axios.post('http://localhost:9292/api/v1/auth/verify', {
-//         code: verificationCode
-//       });
-
-//       // If verification is successful, redirect to the login page
-//       navigate('/login');
-//     } catch (error) {
-//       // Handle error if verification fails
-//       console.error('Verification failed:', error.response.data.message);
-//       // Handle error message (display to the user, etc.)
-//     }
-//   };
-
-//   return (
-//     <div>
-//       <h2>Verification</h2>
-//       <p>Please enter the verification code sent to your email or phone number.</p>
-//       <form onSubmit={handleSubmit}>
-//         <div>
-//           <label>Verification Code:</label>
-//           <input type="text" value={verificationCode} onChange={(e) => setVerificationCode(e.target.value)} />
-//         </div>
-//         <button type="submit">Verify</button>
-//       </form>
-//     </div>
-//   );
-// };
-
-// export default VerifyPage;
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-
+import { useAuth } from './App';
 const VerifyPage = ({ email }) => {
   const [verificationCode, setVerificationCode] = useState('');
   const [error, setError] = useState('');
   const navigate = useNavigate();
+  const { sendData } = useAuth();
+
   const handleVerify = async (e) => {
     e.preventDefault();
     try {
       // Make a POST request to the verify API
       const response = await axios.post('http://localhost:9292/api/v1/auth/verify', {
-        email: email,
+        email: sendData,
         verificationCode: verificationCode
       });
       navigate('/login');
